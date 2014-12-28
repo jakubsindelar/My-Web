5448bd30219705affd039fd89a0e084f494d40f3
jakubsindelar <github@heroesofmemory.eu>
2014-12-28 23:51:53 +0100

    My web add

---
+++ app.rb
@@ -0,0 +1,23 @@
+require 'rubygems'
+require 'sinatra/base'
+
+class MyWeb < Sinatra::Base
+
+  configure do
+    set server: 'thin'
+    set port: 3000
+    set port: '0.0.0.0'
+  end
+
+  helpers do
+  end
+
+  get '/' do
+      erb :index
+  end
+
+  get '/znaceni' do
+    erb :znaceni
+  end
+
+end
